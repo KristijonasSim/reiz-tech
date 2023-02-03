a1b38cdf54261186af9eec36e3b8967367c0b724
Kristijonas Simsonas <ksimsonas@gmail.com>
2023-02-03 13:32:46 +0200

    feat: create Styled Filter Buttons

---
+++ src/components/button/StyledFilterButton.jsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+
+export const StyledFilterButton = styled(Button)`
+  background: none rgb(0, 255, 0);
+  font-family: sans-serif;
+  font-weight: 700;
+  padding: 8px 12px;
+  font-size: 1rem;
+  margin: 10px;
+  transition: background-color 0.14s ease 0s, color 0.14s ease 0s;
+  &:hover {
+    background-color: rgb(0, 204, 0);
+    color: rgb(25, 25, 25);
+  }
+`;
