82b6da3b531c61d9232ca002bbcd6d7c9573096d
Kristijonas Simsonas <ksimsonas@gmail.com>
2023-02-03 15:19:01 +0200

    feat: create filter searchParams

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,67 @@ import { useState, useEffect } from "react";
 import { countriesAPI } from "./components/api/countriesAPI";
 import CountriesList from "./components/countriesComponent/countries/CountriesList";
 import { StyledFilterButton } from "./components/button/StyledFilterButton";
+import { useSearchParams } from "react-router-dom";
+
 const App = () => {
-  const [ countries, setCountries ] = useState([])
+  const [countries, setCountries] = useState([]);
+  let [searchParams, setSearchParams] = useSearchParams();
 
-  console.log(countries)
+  console.log(countries);
+  console.log(searchParams);
   useEffect(() => {
-  countriesAPI
-    .getCountries()
-    .then((data) => setCountries(data.data))
-    .catch((erorr) => console.log(erorr));
-  }, [])
+    countriesAPI
+      .getCountries()
+      .then((data) => setCountries(data.data))
+      .catch((erorr) => console.log(erorr));
+  }, []);
 
+  const handleOceaniaParams = () => {
+    let newSearchParams = new URLSearchParams(searchParams);
+    if (!newSearchParams.has("filterOceania")) {
+      newSearchParams.set("filterOceania", "Oceania");
+    } else {
+      if (newSearchParams.get("filterOceania") === "Oceania") {
+        newSearchParams.delete("filterOceania");
+      }
+    }
+    setSearchParams(newSearchParams);
+  };
+
+  const handleLithuaniaParams = () => {
+    let newSearchParams = new URLSearchParams(searchParams);
+    if (!newSearchParams.has("filterLithuania")) {
+      newSearchParams.set("filterLithuania", "Lithuania");
+    } else {
+      if (newSearchParams.get("filterLithuania") === "Lithuania") {
+        newSearchParams.delete("filterLithuania");
+      }
+    }
+    setSearchParams(newSearchParams);
+  };
+
+  const handleSortParams = () => {
+    let newSearchParams = new URLSearchParams(searchParams);
+    if (!newSearchParams.has("filterSortParams")) {
+      newSearchParams.set("filterSortParams", "SortZ-A");
+    } else {
+      if (newSearchParams.get("filterSortParams") === "SortZ-A") {
+        newSearchParams.delete("filterSortParams");
+      }
+    }
+    setSearchParams(newSearchParams);
+  };
 
   return (
     <Box>
       <Box>
-        <StyledFilterButton>Smaller than Lithuania</StyledFilterButton>
-        <StyledFilterButton>Oceania</StyledFilterButton>
-        <StyledFilterButton>Z-A</StyledFilterButton>
+        <StyledFilterButton onClick={handleLithuaniaParams}>
+          Smaller than Lithuania
+        </StyledFilterButton>
+        <StyledFilterButton onClick={handleOceaniaParams}>
+          Oceania
+        </StyledFilterButton>
+        <StyledFilterButton onClick={handleSortParams}>Z-A</StyledFilterButton>
       </Box>
       <CountriesList countries={countries} />
     </Box>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider
+    router={router}
+    fallbackElement={<h1>Loading</h1>}
+  />
 );
-
